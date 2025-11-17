07fc1678965cfa5a48e96432b69a37d165437196
akashbatham <akashbatham2811@gmail.com>
2025-11-17 15:24:14 +0530

    new update

---
+++ test/list.spec.ts
@@ -0,0 +1,28 @@
+import {test, Browser, Page, chromium, BrowserContext} from "@playwright/test";
+
+test('working list', async ({}) => {
+    const browser:BrowserContext = await chromium.launchPersistentContext('',{headless:false});
+    
+    //getting the list of pages or tabs in the browser
+    const pages = browser.pages();
+    const page:Page = pages[0];
+
+
+    await page.goto('https://demo.playwright.dev/todomvc/#/');
+    const addtodo = page.getByPlaceholder('What needs to be done?');
+    await addtodo.fill('Bread');
+    await page.keyboard.press('Enter'); //Enter key is used to perform the Enter keyboard action.
+    await addtodo.fill('Butter');
+    await page.keyboard.press('Enter');
+    await addtodo.fill('Milk'); //fill is used to fill the input field with the given text.
+    await page.keyboard.press('Enter');
+    await addtodo.fill('Car wash');
+    await page.keyboard.press('Enter');
+    await addtodo.fill('Bike servicing');
+    await page.keyboard.press('Enter');
+    
+    const list = page.getByTestId('todo-item');
+    const name = list.filter({hasText:'Milk'});
+
+
+});
